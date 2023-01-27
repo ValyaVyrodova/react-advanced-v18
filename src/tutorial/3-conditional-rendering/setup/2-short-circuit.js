6a5ef1beff170d7ed583103f71a45f775d4e2d17
Valentyna Vyrodova <valya.vyrodova@gmail.com>
2023-01-27 18:05:52 +0200

    some old changes

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,7 +6,23 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
+
+
+  return (
+  <>
+    {/* <h1>{firstValue}</h1>
+    <h1>value : {secondValue}</h1> */}
+    <h1>{text || 'john doe'}</h1>
+    <button className='btn' onClick={()=> setIsError(!isError)}>toggle error</button>
+    {isError && <h1>Error...</h1> }
+    {isError ? <p>there is an error...</p> : <div>
+      <h2>there is no error</h2></div>}
+  </>
+  );
 };
 
 export default ShortCircuit;
